ac874ca413ca77d4ec60e5a0b8394a442fab764d
Malyshko Vladislav <vladislavm@wizardsdev.com>
2020-12-01 13:14:51 +0200

    added default files to project

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+// JQ
+
+
+$(document).ready(function(){
+	// $('.wrapper-slider.bot').slick({
+	// 	dots: false,
+	// 	arrows: true,
+	// 	centerMode: true,
+	// 	slidesToShow: 3,
+	// 	// centerPadding: 0,
+	// 	variableWidth: true,
+	// 		slidesToScroll: 1,
+ //  		prevArrow: $('.bottom-slider .arrow-left'),
+	// 	nextArrow: $('.bottom-slider .arrow-right'),
+ //  		swipe: false
+	// });
+}) 
+
+
+
+// NATIVE JS
+
+// ...
